b14aaf353b8024824a436039f0c79ac9c4403293
RofaelBehnam <rofaelemil@gmail.com>
2020-06-02 16:37:09 +0400

    Initial project

---
+++ PatentIndexer/src/main/java/com/rofaelemil/patentsindexer/PatentIndexerApplication.java
@@ -0,0 +1,13 @@
+package com.rofaelemil.patentsindexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatentIndexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatentIndexerApplication.class, args);
+	}
+
+}
---
+++ PatentIndexer/src/test/java/com/rofaelemil/patentsindexer/PatentIndexerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rofaelemil.patentsindexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatentIndexerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
